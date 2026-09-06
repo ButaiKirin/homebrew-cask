40580f3e672e665f130dfe437fc0f7936a2cc943
Bevan Kay <email@bevankay.me>
2026-09-07 00:54:21 +1000

    macloggerdx: remove checksum

--- Casks/m/macloggerdx.rb
+++ Casks/m/macloggerdx.rb
@@ -1,6 +1,6 @@
 cask "macloggerdx" do
   version "6.62"
-  sha256 "78b19625c3dbf04cf8fc0dace65a1ed6093936525f0904881916e9bdf63171cb"
+  sha256 :no_check # required as upstream package is often updated in-place
 
   url "https://www.dogparksoftware.com/files/MacLoggerDX#{version.no_dots}.dmg"
   name "MacLoggerDX"
